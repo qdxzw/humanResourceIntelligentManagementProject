cba4738e2ed295ad48b22b20198b148c2c192c3b
xiaoxiong <1651770254@qq.com>
2023-12-17 16:00:33 +0800

    自定义结构获取作用域插槽数据

--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -1,12 +1,95 @@
 <template>
   <div class="container">
     <div class="app-container">
-      组织架构
+      <!-- 展示树形结构 -->
+      <!-- default-expand-all默认展开所有节点 -->
+      <el-tree default-expand-all :data="depts" :props="defaultProps">
+        <!-- 节点结构 -->
+        <!-- v-slot="{node,data}"只能作用在template -->
+        <template v-slot="{ data }">
+          <el-row
+            style="width: 100%; height: 40px"
+            type="flex"
+            justify="space-between"
+            align="middle"
+          >
+            <el-col>{{ data.name }}</el-col>
+            <el-col :span="4"
+              ><span class="tree-manager">{{ data.managerName }}</span>
+              <el-dropdown>
+                <!-- 显示区域内容 -->
+                <span class="el-dropdown-link">
+                  操作<i class="el-icon-arrow-down el-icon--right" />
+                </span>
+                <!-- 下拉菜单内容 -->
+                <el-dropdown-menu slot="dropdown">
+                  <el-dropdown-item>添加子部门</el-dropdown-item>
+                  <el-dropdown-item>编辑部门</el-dropdown-item>
+                  <el-dropdown-item>删除</el-dropdown-item>
+                </el-dropdown-menu>
+              </el-dropdown>
+            </el-col>
+          </el-row></template
+        >
+      </el-tree>
     </div>
   </div>
 </template>
 <script>
 export default {
-  name: 'Department'
+  name: 'Department',
+  data () {
+    return {
+      depts: [
+        {
+          name: '传智教育',
+          managerName: '管理员',
+          children: [
+            {
+              name: '总裁办',
+              managerName: '张三'
+            },
+            {
+              name: '行政部',
+              managerName: '李四'
+            },
+            {
+              name: '人事部',
+              managerName: '王五',
+              children: [
+                {
+                  name: '财务核算部',
+                  managerName: '杜威'
+                },
+                {
+                  name: '税务管理部',
+                  managerName: '杨旭'
+                },
+                {
+                  name: '薪资管理部',
+                  managerName: '李生根'
+                }
+              ]
+            }
+          ]
+        }
+      ],
+      defaultProps: {
+        label: 'name', // 要显示的字段的名字
+        children: 'children' // 读取子节点的字段名
+      }
+    }
+  }
 }
 </script>
+<style scoped>
+.app-container {
+  padding: 30px 140px;
+  font-size: 14px;
+}
+.tree-manager {
+  width: 50px;
+  display: inline-block;
+  margin: 10px;
+}
+</style>
