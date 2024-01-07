28e57a150d5d45eb128ac6ee6b74bbabba3d3e82
xiaoxiong <1651770254@qq.com>
2024-01-07 10:13:59 +0800

    实现员工的页面结构

--- src/views/employee/index.vue
+++ src/views/employee/index.vue
@@ -1,12 +1,61 @@
 <template>
   <div class="container">
     <div class="app-container">
-      员工
+      <div class="left">
+        <el-input
+          style="margin-bottom: 10px"
+          type="text"
+          prefix-icon="el-icon-search"
+          size="small"
+          placeholder="输入员工姓名全员搜索"
+        />
+        <!-- 树形组件 -->
+      </div>
+      <div class="right">
+        <el-row class="opeate-tools" type="flex" justify="end">
+          <el-button size="mini" type="primary">添加员工</el-button>
+          <el-button size="mini">excel导入</el-button>
+          <el-button size="mini">excel导出</el-button>
+        </el-row>
+        <!-- 表格组件 -->
+        <!-- 分页 -->
+      </div>
     </div>
   </div>
 </template>
+
 <script>
 export default {
   name: 'Employee'
 }
 </script>
+
+<style lang="scss" scoped>
+.app-container {
+  background: #fff;
+  display: flex;
+  .left {
+    width: 280px;
+    padding: 20px;
+    border-right: 1px solid #eaeef4;
+  }
+  .right {
+    flex: 1;
+    padding: 20px;
+    .opeate-tools {
+      margin: 10px;
+    }
+    .username {
+      height: 30px;
+      width: 30px;
+      line-height: 30px;
+      text-align: center;
+      border-radius: 50%;
+      color: #fff;
+      background: #04c9be;
+      font-size: 12px;
+      display: inline-block;
+    }
+  }
+}
+</style>
