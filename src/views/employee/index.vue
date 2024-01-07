614583c62656c9278c3df0e2435b67973c01f20a
xiaoxiong <1651770254@qq.com>
2024-01-07 15:01:45 +0800

    员工表格和分页结构

--- src/views/employee/index.vue
+++ src/views/employee/index.vue
@@ -30,7 +30,26 @@
           <el-button size="mini">excel导出</el-button>
         </el-row>
         <!-- 表格组件 -->
+        <el-table>
+          <el-table-column align="center" label="头像" />
+          <el-table-column label="姓名" />
+          <el-table-column label="手机号" sortable />
+          <el-table-column label="工号" sortable />
+          <el-table-column label="聘用形式" />
+          <el-table-column label="部门" />
+          <el-table-column label="入职时间" sortable />
+          <el-table-column label="操作" width="280px">
+            <template>
+              <el-button size="mini" type="text">查看</el-button>
+              <el-button size="mini" type="text">角色</el-button>
+              <el-button size="mini" type="text">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
         <!-- 分页 -->
+        <el-row style="height: 60px" align="middle" type="flex" justify="end">
+          <el-pagination layout="total,prev, pager, next" :total="1000" />
+        </el-row>
       </div>
     </div>
   </div>
