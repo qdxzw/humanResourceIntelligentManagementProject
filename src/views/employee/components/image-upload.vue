9c3be10664e7e1c3713ffe4d093b4bff6d5e0a28
xiaoxiong <1651770254@qq.com>
2024-01-19 01:18:47 +0800

    使用cos-sdk完成图片上传

--- src/views/employee/components/image-upload.vue
+++ src/views/employee/components/image-upload.vue
@@ -6,6 +6,7 @@
     action=""
     :show-file-list="false"
     :before-upload="beforeAvatarUpload"
+    :http-request="uploadImage"
   >
     <img v-if="value" :src="value" class="avatar" />
     <i v-else class="el-icon-plus avatar-uploader-icon" />
@@ -13,6 +14,7 @@
 </template>
 
 <script>
+import COS from 'cos-js-sdk-v5'
 export default {
   props: {
     value: {
@@ -38,6 +40,33 @@ export default {
         this.$message.error('上传头像图片大小不能超过 5MB!')
       }
       return isJPG && isLt2M
+    },
+    // 选择图片上传
+    uploadImage (params) {
+      console.log(params.file)
+      // 完成cos对象的初始化
+      const cos = new COS({
+        SecretId: 'AKIDKcpeOzT1kNtXJ40slgYM3Xr6A6xeP8nM',
+        SecretKey: 'ZzFOOG2tpX1HMEuOkQGyY7Zt8VpHoHi6'
+      })
+      cos.putObject(
+        {
+          Bucket: 'intelligentmanagement-1306913843', // 存储桶名称
+          Region: 'ap-nanjing', // 地域名称
+          Key: params.file.name, // 文件名称
+          StorageClass: 'STANDARD', // 固定值
+          Body: params.file // 文件对象
+        },
+        (err, data) => {
+          if (data.statusCode === 200 && data.Location) {
+            // 拿到了腾讯会返回的地址
+            // 通过input自定义事件将地址传出去
+            this.$emit('input', 'http://' + data.Location) // 将地址返回了
+          } else {
+            this.$message.error(err.message) // 上传失败提示消息
+          }
+        }
+      )
     }
   }
 }
