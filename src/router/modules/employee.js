64e162515a4b223c7a76ca1b3daff62b307acfe7
xiaoxiong <1651770254@qq.com>
2024-01-18 15:05:27 +0800

    员工详情的结构和路由配置

--- src/router/modules/employee.js
+++ src/router/modules/employee.js
@@ -2,13 +2,23 @@ import layout from '@/layout'
 export default {
   path: '/employee',
   component: layout,
-  children: [{
-    path: '',
-    name: 'employee',
-    component: () => import('@/views/employee'),
-    meta: {
-      title: '员工',
-      icon: 'people'
+  children: [
+    {
+      path: '',
+      name: 'employee',
+      component: () => import('@/views/employee'),
+      meta: {
+        title: '员工',
+        icon: 'people'
+      }
+    },
+    {
+      path: '/employee/detail', // 员工详情的地址
+      component: () => import('@/views/employee/detail.vue'),
+      hidden: true, // 表示隐藏在左侧菜单
+      meta: {
+        title: '员工详情'
+      }
     }
-  }]
+  ]
 }
---
+++ src/views/employee/detail.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="dashboard-container">
+    <div class="app-container">
+      <div class="edit-form">
+        <el-form ref="userForm" label-width="220px">
+          <!-- 姓名 部门 -->
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="姓名" prop="username">
+                <el-input size="mini" class="inputW" />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <!-- 工号 入职时间 -->
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="工号" prop="workNumber">
+                <el-input size="mini" class="inputW" />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <!--手机 聘用形式  -->
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="手机" prop="mobile">
+                <el-input size="mini" class="inputW" />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="部门" prop="departmentId">
+                <!-- 放置及联部门组件 -->
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="聘用形式" prop="formOfEmployment">
+                <el-select size="mini" class="inputW" />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="入职时间" prop="timeOfEntry">
+                <el-date-picker
+                  size="mini"
+                  type="date"
+                  value-format="yyyy-MM-dd"
+                  class="inputW"
+                />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="转正时间">
+                <el-date-picker size="mini" type="date" class="inputW" />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <!-- 员工照片 -->
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="员工头像">
+                <!-- 放置上传图片 -->
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <!-- 保存个人信息 -->
+          <el-row type="flex">
+            <el-col :span="12" style="margin-left: 220px">
+              <el-button size="mini" type="primary">保存更新</el-button>
+            </el-col>
+          </el-row>
+        </el-form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang="scss">
+.edit-form {
+  background: #fff;
+  padding: 20px;
+  .inputW {
+    width: 380px;
+  }
+}
+</style>
