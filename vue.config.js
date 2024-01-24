9bf3564c4111353b7c9baf04dfefd765a8a0d9f4
xiaoxiong <1651770254@qq.com>
2024-01-25 00:25:45 +0800

    在 vue.config.js 中设置正确的 publicPath

--- vue.config.js
+++ vue.config.js
@@ -24,7 +24,10 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: '/',
+  publicPath:
+    process.env.NODE_ENV === 'production'
+      ? '/humanResourceIntelligentManagementProject/'
+      : '/',
   outputDir: 'dist',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
